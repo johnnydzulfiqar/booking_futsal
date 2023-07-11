aeaa5bdadd9a703657af2fa86bf0d9be35d12d2e
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2023-07-11 11:10:21 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
